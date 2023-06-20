8df7f51d9ff9c737d819ee00141666221615d15f
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-20 16:15:26 -0300

    Refatoração de código.

--- Python_exercicios/Ex008.py
+++ Python_exercicios/Ex008.py
@@ -2,4 +2,4 @@
 
 valor = float(input('Digite um valor em metros que deseja converter: '))
 
-print('O valor {}m, convertido para cemtimetros é {}cm\ne milimetros é {}mm'.format(valor, valor*100, valor*1000))
+print('O valor {:.2f}m, convertido para cemtimetros é {:.2f}cm\ne milimetros é {:.2f}mm'.format(valor, valor*100, valor*1000))
