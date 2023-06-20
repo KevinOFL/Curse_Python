aa7479ed9d6846c77a0f7f998e2f76fb423f15ed
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-20 16:27:23 -0300

    Conclusão do Ex013.
    Conclusão do exercício 013 onde deveria ser criado um código para dar um aumento de 15% no salario digitado pelo usuário.

---
+++ Python_exercicios/Ex013.py
@@ -0,0 +1,6 @@
+#Desafio aumento de 15% no sálario
+
+sal = float(input('Digite seu sálario: '))
+
+print('Parabéns você acaba de receber 15% de aumento no seu sálario!!!')
+print('Sálario antigo era R${:.2f}, agora passou a ser R${:.2f}.\nParabéns !!'.format(sal, sal+(sal*0.15)))
