8bb0c8bb8daa446af734dffeabaeb8b86765d1c4
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-20 16:11:05 -0300

    Refatoração de código

--- Python_exercicios/Ex009.py
+++ Python_exercicios/Ex009.py
@@ -2,4 +2,6 @@
 
 num = int(input('Digite um número: '))
 
+print('-' * 20)
 print('A tabuada de {} é:\n vezes 1 = {}\n vezes 2 = {}\n vezes 3 = {}\n vezes 4 = {}\n vezes 5 = {}\n vezes 6 = {}\n vezes 7 = {}\n vezes 8 = {}\n vezes 9 = {}\n vezes 10 = {}'.format(num, num*1, num*2, num*3, num*4, num*5, num*6, num*7, num*8, num*9, num*10))
+print('-' * 20)
