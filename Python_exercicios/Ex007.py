109fb2bfd1156209430321d74efb6211e4c80852
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-20 16:07:01 -0300

    Refatoração de código.

--- Python_exercicios/Ex007.py
+++ Python_exercicios/Ex007.py
@@ -3,4 +3,6 @@
 nota1 = float(input('Digite sua 1º nota: '))
 nota2 = float(input('Digite sua 2º nota: '))
 
-print('1º nota é {}\n2º nota é {}\na sua média foi {}.'.format(nota1, nota2, (nota1+nota2)/2))
+#media = (nota 1 + nota2) / 2
+
+print('1º nota é {}\n2º nota é {}\na sua média foi {:.2f}.'.format(nota1, nota2, (nota1+nota2)/2))
