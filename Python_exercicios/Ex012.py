d8be28b3d9f9398d784c744b0c7c27a396105a96
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-20 16:20:37 -0300

    Refatoração de código.

--- Python_exercicios/Ex012.py
+++ Python_exercicios/Ex012.py
@@ -2,4 +2,4 @@
 
 valor = float(input('Digite o valor do produto: '))
 
-print('Você acaba de receber 5% de descnoto nesse produto que custa R${}.\nO valor dele agora é R${}'.format(valor, valor-(valor*0.05)))
+print('Você acaba de receber 5% de desconto nesse produto que custa R${:.2f}.\nO valor dele agora é R${:.2f}'.format(valor, valor-(valor*0.05)))
