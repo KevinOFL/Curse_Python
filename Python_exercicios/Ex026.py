5e563d3846b27147df0906c6784d6df795977119
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-22 17:42:52 -0300

    Refatoração de código

--- Python_exercicios/Ex026.py
+++ Python_exercicios/Ex026.py
@@ -4,5 +4,5 @@ frase = input('Digite uma frase: ')
 
 
 print('A letra A aparece {} vezes na frase: {}.'.format(frase.count('a'), frase))
-print('A primeira posição da letra A é : {}'.format(frase.find('a')))
-print('A ultima posição da letra A é : {}'.format(frase.rfind('a')))
+print('A primeira posição da letra A é : {}'.format(frase.find('a')+1))
+print('A ultima posição da letra A é : {}'.format(frase.rfind('a')+1))
