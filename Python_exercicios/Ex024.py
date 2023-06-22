e591ba993d48ec3f4f2f55e62f4ff0f7b7af1c2e
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-22 16:38:59 -0300

    Conclusão do Ex024
    Finalizei o exercício 024 onde fiz um programa que verificava se a cidade digitada pelo usuário era Santos ou não com o auxilio do operador 'in'.

---
+++ Python_exercicios/Ex024.py
@@ -0,0 +1,6 @@
+#Desafio é Santos ou não
+
+cidade = input('Digite o nome de uma cidade: ')
+
+print('Essa cidade se chama Santos?\n{}'.format('Santos' in cidade))
+#Ultilzei o operador in para me dar True ou False se é ou não Santos.
