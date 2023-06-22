58bd0049a71f1a0b5307c20e186ca76b971d7349
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-22 16:44:34 -0300

    Conclusão do Ex025
    Finalizei o exercício 025 onde fiz um programa que era praticamente igual ao exercício 024 que verifiquei se tinha Silva no nome.

---
+++ Python_exercicios/Ex025.py
@@ -0,0 +1,6 @@
+#Desafio Tem Silva ou não ?
+
+nome = input('Digite o seu nome completo: ')
+
+print('Seu nome possui Silva ?\n{}'.format('Silva' in nome))
+#Ultilzei o operador in para me dar True ou False se é ou não Silva.
