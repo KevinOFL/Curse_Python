8364dfe4d33ff3d38ec05c8be70f7706afcda4a9
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-21 22:33:04 -0300

    Refatoração de código

--- Python_exercicios/Ex018.py
+++ Python_exercicios/Ex018.py
@@ -1,7 +1,7 @@
 #Desafio seno, coseno e tangente.
 
-from math import sin, cos, tan
+from math import sin, cos, tan, radians
 
 angulo = int(input('Digite um ângulo: '))
 
-print('O ângulo que você digitou {}º deu o valor de:\n{}\n seno: {}\n cosseno: {}\n tangente: {}\n{}'.format(angulo, ('-'*50), sin(angulo), cos(angulo), tan(angulo), ('-'*50)))
+print('O ângulo que você digitou {}º deu o valor de:\n{}\n seno: {:.2f}\n cosseno: {:.2f}\n tangente: {:.2f}\n{}'.format(angulo, ('-'*50), sin(radians(angulo)), cos(radians(angulo)), tan(radians(angulo)), ('-'*50)))
