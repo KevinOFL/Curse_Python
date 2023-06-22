92f618cc1c2d9f44368b01477e4b7bca87a0a884
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-22 17:12:25 -0300

    Conclusão do Ex026
    Finalizei o exercício 026 onde fiz um programa que pega quantas vezes aparecia a letra A numa frase digitada pelo usuário e mostrasse o primeiro índice e o ultimo índice da letra A.

---
+++ Python_exercicios/Ex026.py
@@ -0,0 +1,8 @@
+#Desafio mostre
+
+frase = input('Digite uma frase: ')
+
+
+print('A letra A aparece {} vezes na frase: {}.'.format(frase.count('a'), frase))
+print('A primeira posição da letra A é : {}'.format(frase.find('a')))
+print('A ultima posição da letra A é : {}'.format(frase.rfind('a')))
