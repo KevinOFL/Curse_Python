df69b2c0aad521047eccb331b7cf8dbb25b7fd6b
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-23 11:55:34 -0300

    Conclusão do Ex029
    Finalizei o exercicio 029 onde fiz um programa onde o usuário digitava o Km de um carro e o programa verificava se ele estava acima da média permitida (80Km) ou não e aplicava uma multa de R$7.00 para cada Km acima do permitido.

---
+++ Python_exercicios/Ex029.py
@@ -0,0 +1,15 @@
+#Desafio limite de velocidade
+
+veloc = int(input('Digite a velocidade que você está dirigindo: '))
+
+#Variavel para somar o valor da multa.
+#veloc - 80 serve para retirar o km permitido e somar só o resto que veio acima do limite.
+multa = float((veloc - 80) * 7.0)
+
+#Sistema condicional para verificar a velocidade e aplicar a multa.
+if veloc >= 80:
+    print('''Você acaba de tomar uma multa por estar dirigindo acima de 80Km por hora.
+A multa vai custar R$7.00 por cada Km acima do permitido.
+Então a multa ficou no valor de R${}'''.format(multa))
+else:
+    print('Tudo certo senhor, pode seguir.')
