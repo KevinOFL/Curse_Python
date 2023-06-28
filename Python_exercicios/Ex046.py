20e0324c6b7e1d5fe90023ca524da4dd1a820774
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-28 19:12:24 -0300

    Refatoração de código

--- Python_exercicios/Ex046.py
+++ Python_exercicios/Ex046.py
@@ -2,6 +2,10 @@
 
 from time import sleep
 
+print('=-'*36)
+print(' '*25,'\033[7mContagem regressiva\033[m')
+print('=-'*36)
+
 for c in range(10, -1, -1):
     print(c)
     sleep(1)
--- Python_exercicios/Ex047.py
+++ Python_exercicios/Ex047.py
@@ -1,4 +1,8 @@
 #Desafio números pares.
 
+print('=-'*36)
+print(' '*28,'\033[7mNúmeros par\033[m')
+print('=-'*36)
+
 for c in range(2, 50+1, 2):
     print('Número par {}{}{}'.format('\033[32;1m',c,'\033[m'))
