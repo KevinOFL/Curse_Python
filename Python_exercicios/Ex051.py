e01ae92049a867e27a35d1fa37b739159b51461b
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-29 09:57:35 -0300

    Conclusão do Ex051
    Finalizei o exercicio 051 onde fiz um programa que faz um progressão aritmética onde o usuário escolha o termo e a razão e o programa mostra os 10 primeiros termos da PA.

---
+++ Python_exercicios/Ex051.py
@@ -0,0 +1,16 @@
+#Desafio PA
+
+print('=-'*36)
+print(' '*30,'\033[7mProgressão Aritimética (PA)\033[m')
+print('=-'*36)
+
+termo = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razão: '))
+
+print('=-'*36)
+
+print('Os 10 primeiros termos dessa progressão: ')
+for c in range(termo, razao*10+1, razao):
+    print('\033[34;1m{}\033[m'.format(c))
+
+print('=-'*36)
