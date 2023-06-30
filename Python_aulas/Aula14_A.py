d4d4f5d7d1c0a3322db98e005e3e35a4a62e3257
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-29 22:22:15 -0300

    Conclusão da Aula14_A
    Finalizei a aula 14, onde pratiquei e vi algumas teorias sobre o laço while e como utilizar ele de diferentes formas.

---
+++ Python_aulas/Aula14_A.py
@@ -0,0 +1,30 @@
+#Aula sobre while
+
+#for c in range(1, 10):
+#    print(c)
+#print('FIM')
+
+c = 1
+while c < 10:
+    print(c)
+    c+=1
+print('FIM')
+
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um número: '))
+    r = input('Quer continuar ? [S/N]').upper()
+print('FIM')
+
+num = 1
+par = impar = 0
+
+while num != 0:
+    num = int(input('Digite um valor: '))
+    if num != 0:
+        if num % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+
+print('Você digitou {} números pares e {} números impares.'.format(par, impar))
