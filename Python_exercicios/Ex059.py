42b7a26d68e8eb84a3d700c82de3c59456b82dbc
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-30 15:20:09 -0300

    Conclusão do Ex059
    Finalizei o exercicio 059, onde programei um menu com varias funções simples com while que só aceita as opções referente as do menu.

---
+++ Python_exercicios/Ex059.py
@@ -0,0 +1,42 @@
+#Desafio menu de operações.
+
+print('=-'*36)
+print('\033[7;1mMneu de operações\033[m')
+print('=-'*36)
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+opc = 0
+
+print('=-'*36)
+
+while opc != 1 and opc != 2 and opc != 3 and opc != 4 or opc == 5:
+    opc = int(input('''---------- Menu ----------
+    [1] Somar
+    [2] Multiplicar
+    [3] Maior
+    [4] novos números
+    [5] sair do programa
+    Digite operação desejada: '''))
+
+    if opc == 1:
+        print('A soma de {} + {} = {}.'.format(n1, n2, n1 + n2))
+    elif opc == 2:
+        print('A multiplicação de {} * {} = {}.'.format(n1, n2, n1 * n2))
+    elif opc == 3:
+        if n1 > n2:
+            print('{} é maior que {}'.format(n1, n2))
+        elif n1 < n2:
+            print('{} é maior que {}'.format(n2, n1))
+        else:
+            print('{} e {} são valores iguais'.format(n1, n2))
+    if opc == 4:
+        n1 = int(input('Digite um número: '))
+        n2 = int(input('Digite outro número: '))
+    if opc == 5:
+        break
+
+print('Tenha um otimo dia!')
+
+print('=-'*36)
