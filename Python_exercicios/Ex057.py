2b40ad2714aa089d9b4d23aaf2fc6a59559a6289
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-30 14:24:13 -0300

    Conclusão do Ex057
    Finalizei o exercicio 057, onde fiz um programa com laço while que forçava o usuário a digitar somente valores corretos M ou F para poder finalizar o loop.

---
+++ Python_exercicios/Ex057.py
@@ -0,0 +1,14 @@
+#Desafio M ou F.
+
+print('=-'*36)
+print(' '*32,'\033[7mM ou F\033[m')
+print('=-'*36)
+
+sexo = ''
+
+while sexo != 'm' and sexo != 'f':
+    sexo = str(input('Qual o seu sexo? [M/f]'))
+
+print('=-'*36)
+
+print('Obrigado!')
