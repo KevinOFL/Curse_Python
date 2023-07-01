498b3d4c817dca4b575c9b21fc7b99df6ecef652
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-30 21:32:40 -0300

    Conclusão do Ex063
    Finalizei o exercicio 063, onde fiz um programa executar a sequência de Fibonacci.

---
+++ Python_exercicios/Ex063.py
@@ -0,0 +1,22 @@
+#Desafio sequência de Fibonacci
+
+print('=-'*36)
+print(' '*22,'\033[7mSequência de Fibonacci\033[m')
+print('=-'*36)
+
+n1 = int(input('Digite um número inteiro: '))
+
+t1 = 0
+t2 = 1
+cont = 0
+
+print('{} -> {} '.format(t1, t2), end='')
+
+while cont <= n1:
+    t3 = t1 + t2
+    print('-> {} '.format(t3),end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+
+print('FIM')
