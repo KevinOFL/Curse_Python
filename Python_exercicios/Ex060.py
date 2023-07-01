c48290124bd43898a56020a2e1d5e309745078a4
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-06-30 21:00:44 -0300

    Refatoração do código

--- Python_exercicios/Ex060.py
+++ Python_exercicios/Ex060.py
@@ -4,19 +4,14 @@ print('=-'*36)
 print(' '*29,'\033[7;1m Fatorial v2.0 \033[m')
 print('=-'*36)
 
-soma = 0
-cont = 0
-
 usu = int(input('Digite um número inteiro: '))
-usu1 = usu
 
-f = usu-1
+c = usu
+f = 1
 
-while f != 0:
-    soma = usu * f
-    print('{} * {} = {}'.format(usu,f,soma))
-    usu = soma
-    f -= 1
-    cont += soma
-
-print('O fatorial de {} é {}'.format(usu1, cont))
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
