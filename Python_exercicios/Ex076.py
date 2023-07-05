ea220f66804e72a3549384dbaf4bad424587d7cc
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-07-05 11:23:23 -0300

    Refatoração de código

--- Python_exercicios/Ex076.py
+++ Python_exercicios/Ex076.py
@@ -22,6 +22,6 @@ preço = tupla_produtos[0][1]
 for c in tupla_produtos:
     nome = c[0]
     preço = c[1]
-    print(f'{nome:.<30}R${preço}')
+    print(f'{nome:.<30}R${preço:>7.2f}')
 
 print('=-'*36)
