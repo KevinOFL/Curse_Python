2563ca064709e3b0dde5fab3967868ff96e76cee
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-07-06 15:25:27 -0300

    Conclusão do Ex079
    Finalizei o exercicio 079, onde fiz um programa que o usuário pode digitar vários valores mas não pode adicionar o mesmo valor a lista só pode ter valores únicos. No final mostra a lista em ordem crecente.

---
+++ Python_exercicios/Ex079.py
@@ -0,0 +1,28 @@
+#Desafio Vários valores numéricos.
+
+print('=-'*36)
+print(' '*19,'\033[7;1m - Vários Valores numéricos  - \033[m')
+print('=-'*36)
+
+valores = list()
+num = cont = aux = 0
+
+while True:
+    num = input("Digite um valor numérico (ou 'sair' para encerrar): ")
+
+    if num.lower() == 'sair':
+        break
+
+    if num.isdigit():
+        num = int(num)
+        if num not in valores:
+            valores.append(num)
+    else:
+        print("Valor inválido. Digite apenas valores numéricos.")
+
+valores.sort()
+
+print("Valores únicos digitados, em ordem crescente:")
+for valor in valores:
+    print(valor, end=' - ')
+
