85ddce2f23671173e8669c220fb4b054fb5c39b4
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-07-18 15:31:25 -0300

    Conclusão do Ex113
    Finalizei o exercicio 113, onde fiz um programa que aceita um valor inteiro e outro real caso o usuário digite algo valor fora desses 2 ele entrara em um loop até digitar um valor valido.

---
+++ Python_exercicios/Ex113/Ex113_prog.py
@@ -0,0 +1,15 @@
+# Desafio Leia int e float
+
+import Ex113_func
+
+Ex113_func.apresentação()
+
+inteiro = Ex113_func.leiaInt('Digite um número inteiro: ')
+
+Ex113_func.linha()
+
+real = Ex113_func.leiaFloat('Digite um valor real: ')
+
+print(f'''       - RESULTADO -
+O valor inteiro digitado foi {inteiro} e o valor real digitado foi {real}.''')
+
