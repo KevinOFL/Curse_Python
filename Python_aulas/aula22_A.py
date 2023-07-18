24cc7fc6e81007da25c348a7f5cbd37d7c1fe518
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-07-18 14:39:22 -0300

    Conclusão da aula22
    Finalizei a aula 22, onde fiz uns códigos que utilizavam try-except para captura erros e trata-los.

---
+++ Python_aulas/aula22_A.py
@@ -0,0 +1,19 @@
+# Ultilizaremos a trativa de erro
+# try-except
+
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador: '))
+    r = a / b
+except (ValueError, TypeError):
+    print('Tivemos um problem com os tipos de dados que você digitou.')
+except ZeroDivisionError:
+    print('Não é possivel dividir um número por zero.')
+except KeyboardInterrupt:
+    print('O usuário preferiu não informar os dados.')
+except Exception as erro:
+    print(f'O erro encontrado foi {erro.__class__}')
+else:
+    print(f'O resultado foi {r:.2f}')
+finally:
+    print('Volte sempre! Muito obrigado!')
