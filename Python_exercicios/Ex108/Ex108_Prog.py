f61c74e1ac73d9597ef010f96865f8a63891c6cf
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-07-18 11:56:51 -0300

    Conclusão do Ex108
    Finalizei o exercicio 108, onde reutilizei arquivos do exercicio 107 e adicionei um função de formatação de valor monetário.

---
+++ Python_exercicios/Ex108/Ex108_Prog.py
@@ -0,0 +1,19 @@
+# Desafio Trabalhando com modulos 2.
+
+import Ex108_func
+from Ex108_func import moeda
+
+Ex108_func.apresentação()
+
+n = float(input('Digite um número: R$'))
+t = int(input('Digite a taxa: '))
+
+Ex108_func.linha()
+
+print(f'''              - - RESULTADO - -
+Aumentamos 25% do valor original {moeda(n)} -> {moeda(Ex108_func.aumentar(n, t))}.
+Diminuimos 35% do valor original {moeda(n)} -> {moeda(Ex108_func.diminuir(n, t))}.
+Dobramos o valor original {moeda(n)} -> {moeda(Ex108_func.dobro(n))}.
+Dividimos o valor orinal {moeda(n)} -> {moeda(Ex108_func.metade(n))}.''')
+
+Ex108_func.linha()
