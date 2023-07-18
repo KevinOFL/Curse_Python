85f212d1d82685fb33edf494ec89a4fe0ccc6112
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-07-18 12:24:32 -0300

    Conclusão do Ex110
    Finalizei o exercicio 110, onde reutilizei arquivos do exercicio 109 e criei uma nova função chamada resumo onde praticamente mostrar o resultado do programa.

---
+++ Python_exercicios/Ex110/Ex110_Prog.py
@@ -0,0 +1,12 @@
+# Desafio Trabalhando com modulos 2.
+
+import Ex110_func
+
+Ex110_func.apresentação()
+
+n = float(input('Digite um número: R$'))
+t = int(input('Digite a taxa: '))
+
+Ex110_func.resumo(n,t)
+
+
